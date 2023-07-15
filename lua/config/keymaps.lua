ce33bda0554388eff3c92a789daf001021a922bf
Pavel Pavlov <kx355@mail.ru>
2023-07-15 12:58:55 +0300

    some minor changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,4 +11,5 @@ map("n", "<leader>oo", "<cmd>Neotree toggle<cr>", { desc = "Focus NeoTree" })
 
 map("n", "<leader>.", "<cmd>Neotree buffers focus<cr>", { desc = "Toggle NeoTree buffers" })
 map("n", "<leader>..", "<cmd>Neotree buffers toggle<cr>", { desc = "Toggle NeoTree buffers" })
+
 map("n", "<leader>ge", "<cmd>Neotree git_status toggle<cr>", { desc = "Toggle NeoTree Gitstatus" })
