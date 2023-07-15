6676115e000bfef3e666465ca2b57d561230b131
Pavel Pavlov <kx355@mail.ru>
2023-07-15 13:17:00 +0300

    add hide opts

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,7 +2,33 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      bind_to_cwd = true,
+      bind_to_cwd = false,
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db",
+          "node_modules",
+        },
+        hide_by_pattern = {
+          "*.dSYM",
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show_by_pattern = { -- uses glob style patterns
+          --".null-ls_*",
+        },
+      },
     },
   },
 }
